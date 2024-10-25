f08cc0cd69c88f34a08f09a861cd874893222f43
Karol Piczak <karol@piczak.com>
2024-10-25 13:47:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 rm -rf ~/.local/share/plasma/plasmoids/org.dhruv8sh.kara
-cp -r ../kara ~/.local/share/plasma/plasmoids/org.dhruv8sh.kara
+cp -r ../kde-kara ~/.local/share/plasma/plasmoids/org.dhruv8sh.kara
 systemctl --user restart plasma-plasmashell
